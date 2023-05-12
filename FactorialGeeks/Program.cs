948c11e09c0b83e710ef4d8aac491b59ec9aaec7
Daniel ikechukwu <danielikechukwu495@gmail.com>
2023-05-12 23:32:51 +0100

    Add project files.

---
+++ FactorialGeeks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+	class Factorial
+	{
+		static int factorial(int x)
+		{
+			int r = 1;
+
+			for(int i = 1; i <= x; i++)
+			{
+				//r *= factorial(i);
+
+				//r = r * i;
+
+				r *= i;
+			}
+
+			return r;
+
+
+		}
+		static void Main(string[] args)
+		{
+
+			int d = 5;
+
+			Console.WriteLine("Hello, the Factorial of D ."+" " + factorial(d));
+		}
+
+	}
+}
